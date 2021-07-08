7fde7abb87218a08f915f2e5207038af77574a47
yonePC <ys.mailaddress0929@gmail.com>
2021-07-08 12:11:44 +0000

    非同期いいね実装

--- bookers2/app/controllers/favorites_controller.rb
+++ bookers2/app/controllers/favorites_controller.rb
@@ -4,7 +4,7 @@ class FavoritesController < ApplicationController
   def create
     @book = Book.find(params[:book_id])
     favorite = @book.favorites.new(user_id: current_user.id)
-    favorite.save
+     favorite.save
     # redirect_to request.referer
   end
 
